75e891d986072dc59faff40bff422571e745e5f2
Umut Ercan <umuterc123@gmail.com>
2022-06-11 14:32:42 +0300

    Initial Commit

---
+++ OpenGLAirCinema/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGLAirCinema
+//
+//  Created by Umut Ercan on 11.06.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
